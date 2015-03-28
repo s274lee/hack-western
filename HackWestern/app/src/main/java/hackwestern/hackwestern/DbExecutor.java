2bd09858da923b101cf934cb496410bc209cbc3d
venalt <rlsantos@uwaterloo.ca>
2015-03-28 15:34:18 -0400

    Can now insert mock data to test db

--- HackWestern/app/src/main/java/hackwestern/hackwestern/DbExecutor.java
+++ HackWestern/app/src/main/java/hackwestern/hackwestern/DbExecutor.java
@@ -16,12 +16,12 @@ public class DbExecutor extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQLScripts.SQL_CREATE_ENTRIES);
+        db.execSQL(SQLScripts.SQL_CREATE_MESSAGE_TABLE);
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-        db.execSQL(SQLScripts.SQL_DELETE_ENTRIES);
+        db.execSQL(SQLScripts.SQL_DELETE_MESSAGE_TABLE);
         onCreate(db);
     }
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
--- HackWestern/app/src/main/java/hackwestern/hackwestern/SQLScripts.java
+++ HackWestern/app/src/main/java/hackwestern/hackwestern/SQLScripts.java
@@ -5,8 +5,8 @@ package hackwestern.hackwestern;
  */
 public class SQLScripts {
     public static final String COMMA_SEP = ",";
-    public static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + SQLContract.MessageTable.TABLE_NAME + " (" +
+    public static final String SQL_CREATE_MESSAGE_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + SQLContract.MessageTable.TABLE_NAME + " (" +
                     SQLContract.MessageTable._ID + " INTEGER PRIMARY KEY," +
                     SQLContract.MessageTable.COLUMN_RECIPIENT + " TEXT" + COMMA_SEP +
                     SQLContract.MessageTable.COLUMN_PHONE_NUMBER + " INTEGER" + COMMA_SEP +
@@ -17,6 +17,17 @@ public class SQLScripts {
                     SQLContract.MessageTable.COLUMN_LATITUDE + " REAL" + COMMA_SEP +
             " )";
 
-    public static final String SQL_DELETE_ENTRIES =
+    public static final String SQL_DELETE_MESSAGE_TABLE =
             "DROP TABLE IF EXISTS " + SQLContract.MessageTable.TABLE_NAME;
+
+    public static final String SQL_INSERT_MESSAGE_TABLE = "INSERT INTO " +
+            SQLContract.MessageTable.TABLE_NAME + " (" +
+            SQLContract.MessageTable.COLUMN_RECIPIENT + COMMA_SEP +
+            SQLContract.MessageTable.COLUMN_PHONE_NUMBER + COMMA_SEP +
+            SQLContract.MessageTable.COLUMN_TIME_CREATED + COMMA_SEP +
+            SQLContract.MessageTable.COLUMN_TIME_SENT + COMMA_SEP +
+            SQLContract.MessageTable.COLUMN_SENT_FLAG + COMMA_SEP +
+            SQLContract.MessageTable.COLUMN_LONGITUDE + COMMA_SEP +
+            SQLContract.MessageTable.COLUMN_LATITUDE + COMMA_SEP +
+            ") VALUES ('Sharon Lee', 6472946538, 'null', 'null', 0, 45.4, 75.66)";
 }
